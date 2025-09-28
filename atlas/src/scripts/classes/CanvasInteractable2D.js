d403ed0bfb175cdafba65d476559ee78b9325c7e
ryanrnjit <rjr@njit.edu>
2025-09-28 14:00:52 -0400

    Adding a node

--- atlas/src/scripts/classes/CanvasInteractable2D.js
+++ atlas/src/scripts/classes/CanvasInteractable2D.js
@@ -3,17 +3,38 @@
 */
 
 export class CanvasInteractable2D {
-    constructor(canvasObject) {
+    constructor(canvasObject, x, y, w, h, name=null) {
         this.canvasObject = canvasObject;
         this._listeners = [];
-        canvasObject.interactables.append(this);
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        canvasObject.interactables.push(this);
     }
 
     addEventListener(event, callback) {
-        this._listeners.append({
+        this._listeners.push({
             event: event,
             callback: callback
         });
+        this.canvasObject.addEventListeners(); //refresh listeners
+    }
+
+    addClickListener(callback) {
+        this.addEventListener("click", ()=>{
+            console.log(`(${this.canvasObject.mousepos_x}, ${this.canvasObject.mousepos_y})`);
+            console.log(`mx: ${this.canvasObject.mousepos_x} bx: ${this.x + this.canvasObject.x_offset}`);
+            console.log(`my: ${this.canvasObject.mousepos_y} by: ${this.y + this.canvasObject.y_offset}`);
+            if(this.canvasObject.mousepos_x < this.x + this.canvasObject.x_offset || 
+                this.canvasObject.mousepos_x > this.x + this.canvasObject.x_offset + this.w || 
+                this.canvasObject.mousepos_y < this.y + this.canvasObject.y_offset || 
+                this.canvasObject.mousepos_y > this.y + this.canvasObject.y_offset + this.h
+            ) {
+                return;
+            }
+            callback();
+        })
     }
 
     activateEventListeners() {
--- atlas/src/scripts/view_sandbox.js
+++ atlas/src/scripts/view_sandbox.js
@@ -10,5 +10,5 @@ export function initializeCanvas(canvas, parsed_pdf) {
 
     Sandbox.addEventListeners();
     Sandbox.draw();
-
+    return Sandbox;
 }
