8269c5fc26d942d59ad79b4f8469d55a02c5679d
ryanrnjit <rjr@njit.edu>
2025-10-03 19:44:43 -0400

    Popup for editing node content.

--- atlas/src/scripts/classes/Node.js
+++ atlas/src/scripts/classes/Node.js
@@ -1,21 +1,27 @@
 // Um... am I allowed to call the file this..?
 import { CanvasInteractable2D } from "./CanvasInteractable2D";
+import NodeContent from "../../components/NodeContent";
 
 export class Node {
-    constructor(canvasObj, x, y, r, text="New Node") {
+    constructor(canvasObj, x, y, r, text="New Node", content="") {
         this.canvasObj = canvasObj;
         this.x = x;
         this.y = y;
         this.r = r;
-        this.text = text;
+        this.title = text;
+        this.content = content;
         this.color = "cornflowerblue";
         this.interaction = new CanvasInteractable2D(canvasObj, this.x-this.r, this.y-this.r, this.r*2, this.r*2);
         this.interaction.addClickListener(this.openInspector.bind(this));
     }
 
     setTitle(title) {
-        this.text = title;
+        this.title = title;
         this.canvasObj.draw();
+    }
+
+    setContent(content) {
+        this.content = content;
     }
 
     setColor(color) {
@@ -28,10 +34,21 @@ export class Node {
         this.canvasObj.selectedNode = this;
         document.getElementById("nodeInspector").hidden = false;
         document.getElementById("nodeColorPicker").value = this.color;
-        document.getElementById("nodeNameText").value = this.text; 
+        document.getElementById("nodeNameText").value = this.title;
+        const contentPopup = document.getElementById("node-content-bubble");
+        const contentTextArea = document.getElementById("node-content-text")
+        if(!contentPopup) return;
+        contentTextArea.value = this.content;
+        contentTextArea.addEventListener("change", this.canvasObj._boundUpdateNodeContent)
+        document.getElementById("node-content-title").textContent = this.title;
+        
+        contentPopup.hidden = false;
     }
 
+
+
     drawNode() {
+        const textMargin = 0.25;
         let ctx = this.canvasObj.ctx;
         let titleTextSize = 12;
         ctx.beginPath();
@@ -45,6 +62,6 @@ export class Node {
         ctx.stroke();
         ctx.fillStyle = "black";
         ctx.font = `${titleTextSize}px Arial`;
-        ctx.fillText(this.text, this.x - this.r, this.y, this.r * 2);
+        ctx.fillText(this.title, this.x - this.r*(1-textMargin), this.y, this.r * (2-textMargin*2));
     }
 }
