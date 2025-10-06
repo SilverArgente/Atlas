77229731c2878c982a8401d526fe478fa41c897f
tyang1567 <yangtiffany78@gmail.com>
2025-10-06 18:05:33 -0400

    fixed supabase client, added .env to gitignore

--- atlas/src/services/supabaseClient.js
+++ atlas/src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.SUPABASE_URL;
-const anonKey = process.env.SUPABASE_ANON_KEY;
+const url = process.env.REACT_APP_SUPABASE_URL;
+const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(url, anonKey);
