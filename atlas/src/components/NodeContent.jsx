27ffaa06bb014783861401eb56ec6761aaaad523
ryanrnjit <rjr@njit.edu>
2025-10-08 21:29:51 -0400

    WIP big node styling

--- atlas/src/components/NodeContent.jsx
+++ atlas/src/components/NodeContent.jsx
@@ -7,7 +7,7 @@ export default function NodeContent({title, content}) {
             <div>
                 <h1>Edit content for: <strong onClick={()=>{alert("Please use the Node inspector in the toolbar to edit this (for now.)")}} id="node-content-title"></strong></h1>
                 <textarea id="node-content-text" onChange={(e)=>{setContentText(e.target.value)}} name="nodeContentBox" rows="6" cols="35" placeholder="Enter node content..." value={contentText}></textarea>
-                <img id="node-image" style={{maxWidth: "200pt"}}></img>
+                <img id="node-image" style={{maxWidth: "200pt", maxHeight:"200pt"}}></img>
                 <input type="file" id="node-content-image" accept="image/png, image/jpeg"></input>
                 <button onClick={()=>{document.getElementById("node-content-bubble").hidden = true}} style={{position: 'absolute', top: '5pt', right: '10pt'}}>X</button>
             </div>
