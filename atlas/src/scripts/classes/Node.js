9a562172adcc705392609992592d18a9790e1c96
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-12-03 20:13:08 -0500

    final final fix

--- atlas/src/scripts/classes/Node.js
+++ atlas/src/scripts/classes/Node.js
@@ -170,7 +170,7 @@ export class Node {
         const relatedNodesList = document.getElementById("relatedNodesList");
         const nodeListDropdown = document.getElementById("relatedNodeSelector");
         const buttonList = document.getElementById("RelatedNodeButtons");
-        if (!buttonList) return;
+        // if (!buttonList) return;
         if(this.canvasObj.user_type === "editor") {
             nodeListDropdown.innerHTML = "<option disabled id='default'>Select a Node.</option>"; // Future note (11/19/2025), this was dumb
             document.getElementById("AddRelatedNode").disabled = true;
