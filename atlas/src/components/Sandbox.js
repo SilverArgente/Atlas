6d27ff1d0dd7fae0ca6c316775829f4dabaa6eaa
Amogh Dalal <53583492+SilverArgente@users.noreply.github.com>
2025-12-05 22:37:38 -0500

    fixed centering

--- atlas/src/components/Sandbox.js
+++ atlas/src/components/Sandbox.js
@@ -345,17 +345,17 @@ const handleSaveWithName = async (mapName) => {
         return(
             <div id="toolbar">
                 <h1 className="viewer-menu-title">Atlas Menu</h1>
-                {user && (
-                    <button
-                        id="signOutButton"
-                        onClick={handleSignOut}
-                        className="sign-out-button"
-                        style={{ marginTop: '10px', borderRadius: '6px', padding: '8px 12px' }}
-                    >
-                        Sign Out
-                    </button>
-                )}
                 <div className="viewer-button-container">
+                    {user && (
+                        <button
+                            id="signOutButton"
+                            onClick={handleSignOut}
+                            className="sign-out-button"
+                            style={{ marginTop: '10px', borderRadius: '6px', padding: '8px 12px' }}
+                        >
+                            Sign Out
+                        </button>
+                    )}
                     <button id="refreshSimulationButton" onClick={()=>{canvasObject.restart_simulation()}} className="viewer-button">
                         Restart Simulation
                     </button>
